544eea9f1378d4609d97012cffad9c80a340f1e7
DartS1der <126813579+DartS1der@users.noreply.github.com>
2025-06-20 23:10:50 +0200

    Update auth.py

--- auth.py
+++ auth.py
@@ -2,6 +2,8 @@ from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from typing import Optional
+from fastapi import APIRouter
+
 
 router = APIRouter()
 
