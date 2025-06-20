d1980154606ec9c5f43aea25e716f7931bb387a7
DartS1der <126813579+DartS1der@users.noreply.github.com>
2025-06-20 20:20:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from database import Base, engine
+from auth import router as auth_router
 from esp_api import router as esp_router
+from user_api import router as user_router  # мы его добавим в следующем шаге
 
-app = FastAPI(title="ESP32 Command Server")
+app = FastAPI()
 
+Base.metadata.create_all(bind=engine)
+
+app.include_router(auth_router, prefix="/auth", tags=["Auth"])
 app.include_router(esp_router, prefix="/esp", tags=["ESP32"])
+# app.include_router(user_router, prefix="/user", tags=["User"])
